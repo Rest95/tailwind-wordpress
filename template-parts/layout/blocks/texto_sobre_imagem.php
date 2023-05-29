c6650d44ca2439455242217de7df9db6ce2aefcd
Ricardo Magalhães <ricardo@willbe.co>
2023-05-29 16:11:19 +0100

    small fixes

--- template-parts/layout/blocks/texto_sobre_imagem.php
+++ template-parts/layout/blocks/texto_sobre_imagem.php
@@ -11,7 +11,7 @@ $link_btn = get_sub_field("link_botao");
       <img src="<?php echo $imagem; ?>" alt="<?php echo $titulo; ?>" class="img-fill" />
     </div>
     <div class="relative flex flex-col items-center justify-center space-y-8">
-      <h1 class="text-white text-4xl sm:text-5xl md:text-7xl font-bold uppercase max-w-[300px] md:max-w-[430px] text-center font-roboto"><?php echo $titulo; ?></h1>
+      <h1 class="text-white text-3xl sm:text-4xl md:text-5xl font-bold uppercase max-w-[300px] md:max-w-[430px] text-center font-roboto"><?php echo $titulo; ?></h1>
       <a class="btn btn-negative" href="<?php echo $link_btn ?>"><?php echo $label_btn ?></a>
     </div>
   </div>
--- template-parts/layout/blocks/video_e_texto.php
+++ template-parts/layout/blocks/video_e_texto.php
@@ -4,11 +4,11 @@ $video = get_sub_field("video");
 ?>
 <section class="px-4 md:px-6">
   <div class="aspect-[600/1280] md:aspect-[1280/852] w-full max-h-[90vh] md:max-h-[85vh] relative overflow-hidden">
-    <video autoplay loop playsinline class="w-full h-full object-cover">
+    <video autoplay loop playsinline muted class="w-full h-full object-cover">
       <source src="<?php echo $video ?>" type="video/mp4"/> 
     </video>
     <div class="absolute max-w-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <h1 class="font-roboto font-bold text-4xl sm:text-5xl md:text-7xl uppercase text-center text-white">
+      <h1 class="font-roboto font-bold text-3xl sm:text-4xl md:text-5xl uppercase text-center text-white">
         <?php echo $titulo;?> 
       </h1>
     </div>
